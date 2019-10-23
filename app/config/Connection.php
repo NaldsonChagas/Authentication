adc3673eb9240d78f821754f5270e1b951f58a92
Naldson Chagas <naldson.bc@gmail.com>
2019-10-23 03:34:32 -0300

    Update Connection.php

--- app/config/Connection.php
+++ app/config/Connection.php
@@ -7,7 +7,7 @@ class Connection
     public static function getConnection() 
     {
         $pdo = new \PDO('mysql:dbname=registerAndLogin;host=localhost', 'root',
-            '19982018');
+            '');
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
